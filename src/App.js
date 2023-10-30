0d0b7e52c906536df3c2b5cb8f865000a0ab7c56
Julia K <123kochygova@gmail.com>
2023-10-30 12:30:35 +0300

    m,sdn,vmdsnvdsv,

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 
+import { CrMain } from './components/main/main.jsx';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
       </header>
+      <CrMain/>
     </div>
   );
 }
