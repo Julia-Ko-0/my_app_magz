3df0482eb982800899d37ee9e668bb7d4d983029
Julia K <123kochygova@gmail.com>
2023-10-31 10:24:46 +0300

    ksejfej

---
+++ src/components/main/main_text.jsx
@@ -0,0 +1,25 @@
+import a from './maim.module.css'
+function Text(){
+    return(
+        <div>
+            <h1>Почему выбирают нас</h1>
+            <div>
+                <div>
+                    <h2>скидки постоянным клиентам от 5%
+                    </h2>
+                </div>
+                <div>
+                    <h2></h2>
+                </div>
+                <div>
+                    <h2></h2>
+                </div>
+                <div>
+                    <h2></h2>
+                </div>
+                   
+            </div>
+        </div>
+    )
+}
+export{Text}
