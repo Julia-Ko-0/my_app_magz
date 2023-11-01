d55ce272b52a4c9ad676d8fee953d8f236027713
Julia K <123kochygova@gmail.com>
2023-11-01 13:14:52 +0300

    m,asfncm,ds vc

--- src/components/main/main_text.jsx
+++ src/components/main/main_text.jsx
@@ -1,23 +1,24 @@
 import a from './maim.module.css'
 function Text(){
     return(
-        <div class={a.div_text}>
+        <div class={a.div}>
             <h1>Почему выбирают нас</h1>
             <div class ={a.text}>
-                <div class={a.text1}>
-                    <h2>скидки постоянным клиентам от 5%
-                    </h2>
-
+                <div class={a.text_vs}>
+                    <div class={a.ttt}>
+                        <h2>Скидки постоянным клиентам от 5%
+                        </h2>
+                        </div>
                     <img class={a.im_kr1} src="./imgs/Ellipse 4.svg" alt="" />
                     </div>
-                <div>
+                <div  class={a.text_vs}>
                     <h2>Предлагаем самые выгодные цены</h2>
                 </div>
-                <div>
+                <div  class={a.text_vs}>
                     <h2>Наши покупатели всегда остаются довольны</h2>
                     <div></div>
                 </div>
-                <div>
+                <div class={a.text_vs}>
                     <h2>Ширикий ассортимент товаров для всей семьи</h2>
                 </div>
                    
