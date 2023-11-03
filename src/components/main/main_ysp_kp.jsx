752c10470360a0c956ac774a9f5030a36180eb5e
Julia K <123kochygova@gmail.com>
2023-11-03 12:39:49 +0300

    dsvdsvdsvdsv

--- src/components/main/main_ysp_kp.jsx
+++ src/components/main/main_ysp_kp.jsx
@@ -1,4 +1,7 @@
 import a from './maim.module.css'
+import {Mas} from './mas.jsx'
+import {Pr_ys_kk} from './profile.jsx'
+console.log(Mas[1].price)
 function Ys_kyp(){
     return(
     <div class={a.div}>
@@ -6,10 +9,19 @@ function Ys_kyp(){
             <h1>Успейте купить!</h1>
             <h1>Акции</h1>
         </div>
-        <div>
-
+        <div class={a.tovr}>
+            {
+              Mas.map(m=>(
+                <Pr_ys_kk skl={m.imgs} price={m.price} pr_price={m.pr_price}
+                otdel={m.otdel}
+                ocenk={m.ocenk}/>
+              ))
+            }
         </div>
     </div>
     )
 }
+// {for(let i = 0;i<mas.length ; i++){
+//     console.log(mas[i].price)
+// }}
 export{Ys_kyp}
