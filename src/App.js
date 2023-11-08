0813fb56773b224a2879ffe29b353eabbd50dfeb
Julia K <123kochygova@gmail.com>
2023-11-08 12:59:17 +0300

    dgerdherah

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 import { CrMain } from './components/main/main.jsx';
 import { CrHeder } from './components/heder/heder.jsx';
-
+import { CrCard } from './components/card/card.jsx';
 import { CrKatalog } from './components/katalog/ocn_katal.jsx';
+
 function App() {
   return (
     <div class="App">
       <div class='div'>
         <CrHeder/>
       {/* <CrMain/> */}
-
+        {/* <CrCard/> */}
         <CrKatalog slk="Главная/Женщинам/Блузки и рубашки "/>
       </div>
       
---
+++ src/components/card/cardMas.jsx
@@ -0,0 +1,43 @@
+let MasCard=[
+    {
+        polz:1,
+        img:'./imgs/card_img/Rectangle 62.png',
+        name:'Платье женское',
+        articul:'1248124',
+        color:'Синий',
+        size:"34",
+        kolv:"1",
+        price:1500
+    },
+    {
+        polz:1,
+        img:'./imgs/card_img/Rectangle 63.png',
+        name:'Платье женское',
+        art:1248124,
+        color:'Синий',
+        size:"35",
+        kolv:2,
+        price:1700
+    },
+    {
+        polz:2,
+        img:'./imgs/card_img/Rectangle 63.png',
+        name:'Платье женское',
+        art:1248124,
+        color:'Синий',
+        size:"35",
+        kolv:2,
+        price:1700
+    },
+    {
+        polz:2,
+        img:'./imgs/card_img/Rectangle 63.png',
+        name:'Платье женское',
+        art:1248124,
+        color:'Синий',
+        size:"35",
+        kolv:2,
+        price:1700
+    }
+]
+export{MasCard}
