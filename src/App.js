abee5fc3a53a9d4d1cde4b552bfda4bf75a4f52a
Julia K <123kochygova@gmail.com>
2023-11-08 14:42:16 +0300

    fgcgghfjhg

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { CrMain } from './components/main/main.jsx';
 import { CrHeder } from './components/heder/heder.jsx';
 import { CrCard } from './components/card/card.jsx';
 import { CrKatalog } from './components/katalog/ocn_katal.jsx';
-
+import { CrExt } from './components/exet/ext.jsx';
 function App() {
   return (
     <div class="App">
       <div class='div'>
-        <CrHeder/>
+        <CrExt/>
+        {/* <CrHeder/> */}
       {/* <CrMain/> */}
-        <CrCard/>
+        {/* <CrCard/> */}
         {/* <CrKatalog slk="Главная/Женщинам/Блузки и рубашки "/> */}
       </div>
       
